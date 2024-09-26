1a2bf152af4d55497ef169aa22ce7a757e8f4ded
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-09-26 07:43:33 +0200

    add demo tasks for interacting with environment variables

--- airflow/dags/demo_dag.py
+++ airflow/dags/demo_dag.py
@@ -1,6 +1,7 @@
+import os
 from datetime import datetime
-
-from airflow import DAG
+from airflow.models.dag import DAG
+from airflow.models import Variable
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
@@ -23,8 +24,30 @@ with DAG(dag_id="demo_dag", start_date=datetime.today(), schedule="0 0 * * *") a
     def airflow():
         print("airflow")
 
+    @task
+    def print_env_var():
+        kafka_topic = os.environ.get('COMMENTS_TOPIC_NAME')
+        api_key = os.environ.get('GOOGLE_API_KEY')
+        print(f"{kafka_topic=}")
+        print(f"{api_key=}")
+
+    set_env_var = BashOperator(task_id='set_env_var',
+                               bash_command='export AIRFLOW_VAR_COMMENTS_TOPIC_NAME=$COMMENTS_TOPIC_NAME ' \
+                               '&& echo AIRFLOW_VAR_COMMENTS_TOPIC_NAME=$AIRFLOW_VAR_COMMENTS_TOPIC_NAME ' \
+                               '&& export AIRFLOW_VAR_GOOGLE_API_KEY=$GOOGLE_API_KEY ' \
+                               '&& echo AIRFLOW_VAR_GOOGLE_API_KEY=$AIRFLOW_VAR_GOOGLE_API_KEY')
+
+    @task
+    def print_context_var():
+        kafka_topic = Variable.get('comments_topic_name')
+        api_key = "KEY" # Variable.get('google_api_key')
+        print(f"{kafka_topic=}")
+        print(f"{api_key=}")
+
     # Set dependencies between tasks
     hello >> print_world()
     hello >> print_rochdy()
     hello >> print_airflow()
     hello >> airflow()
+    set_env_var >> print_context_var()
+    print_env_var()
