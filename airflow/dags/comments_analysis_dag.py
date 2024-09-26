2f1f5a4d3dade1fb8dde964abb5b52e26ee1176a
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-09-26 07:42:30 +0200

    add a dag for comments analysis (only containing streaming task)

--- airflow/dags/comments_analysis_dag.py
+++ airflow/dags/comments_analysis_dag.py
@@ -1,11 +1,30 @@
-from airflow import DAG
-from airflow.decorators import task
+from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from utilities import setup_streaming_agent
 
-from .stream_ytb_comments import stream_ytb_comments
+# Default arguments for the DAG
+DAG_DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
 
-with DAG(dag_id='comments_analysis', schedule_interval='@daily', catchup=False):
+# Define comments analysis DAG
+with DAG(
+    dag_id = 'comments_analysis',
+    default_args = DAG_DEFAULT_ARGS,
+    schedule_interval = '@daily',
+    start_date = datetime.today(),
+    catchup = False
+):
+    # Get an agent with appropriate configuration
+    streaming_agent = setup_streaming_agent()
+    # Define data streaming task
     stream_ytb_comments_task = PythonOperator(
-        task_id='stream_ytm_comments',
-        python_callable=stream_ytb_comments
+        task_id='stream_ytb_comments',
+        python_callable=streaming_agent.start
     )
+    
+    # DAG modelisation
+    stream_ytb_comments_task
