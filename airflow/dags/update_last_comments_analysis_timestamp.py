c87217ca5f368ee84217012e4249aa93b6be4b1c
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-10-10 14:20:01 +0200

    add task function to update comments analysis timestamp

--- airflow/dags/update_last_comments_analysis_timestamp.py
+++ airflow/dags/update_last_comments_analysis_timestamp.py
@@ -5,12 +5,22 @@ CASSANDRA_HOST = Variable.get('CASSANDRA_HOST')
 CASSANDRA_KEYSPACE = Variable.get('CASSANDRA_KEYSPACE')
 FETCHING_INFO_TABLE = Variable.get('FETCHING_INFO_TABLE')
 
-cluster = Cluster([CASSANDRA_HOST])
-session = cluster.connect(CASSANDRA_KEYSPACE)
+def update_last_comments_analysis_timestamp(video_id):
+    cluster = Cluster([CASSANDRA_HOST])
+    session = cluster.connect(CASSANDRA_KEYSPACE)
 
-query = 'SELECT video_id, last_fetching_timestamp FROM {}'.format(FETCHING_INFO_TABLE)
-rows = session.execute(query)
+    update_query = 'UPDATE {table_name} SET last_fetching_timestamp = toTimeStamp(now()) WHERE video_id = \'{video_id}\''.format(
+        table_name=FETCHING_INFO_TABLE,
+        video_id=video_id
+    )
+    session.execute(update_query)
 
-for item in rows:
-    print(item.video_id, item.last_fetching_timestamp)
+    read_query = 'SELECT video_id, last_fetching_timestamp FROM {table_name} WHERE video_id = \'{video_id}\''.format(
+        table_name=FETCHING_INFO_TABLE,
+        video_id=video_id
+    )
+    rows = session.execute(read_query)
+
+    for item in rows:
+        print(item.video_id, item.last_fetching_timestamp)
 
